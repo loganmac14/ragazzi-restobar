c44e643716ab4f9d6910fd561084916e949aa6d5
Logan MacDonald <logan.14.mac@gmail.com>
2023-08-15 11:20:41 -0300

    fixed hamburger

--- js/main.js
+++ js/main.js
@@ -3,13 +3,31 @@ const navMenu = document.querySelector("nav ul");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener('click', function () {
+  if (main.classList.contains('hidden')) {
+    main.classList.remove('hidden');
+    footer.classList.remove('hidden');
+    setTimeout(function () {
+      main.classList.remove('active');
+      footer.classList.remove('active');
+    }, 20);
+  } else {
+    main.classList.add('active');
+    footer.classList.add('active');    
+    main.addEventListener('transitionend', function(e) {
+      main.classList.add('hidden');
+      footer.classList.add('hidden');
+    }, {
+      capture: false,
+      once: true,
+      passive: false
+    });
+  }
   header.classList.toggle("active");
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  main.classList.toggle("active");
-  footer.classList.toggle("active");
-})
+}, false);
+
 document.querySelectorAll("nav a").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
